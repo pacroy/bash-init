9d7749eae0a0fe06cc0a434bb611125f4bbefff3
Par <pacroy@gmail.com>
2020-08-19 14:23:31 +0700

    Add gitc, gitp, update-alias

--- alias.sh
+++ alias.sh
@@ -28,3 +28,8 @@ alias gitlog='git log --oneline --decorate --all'
 alias gittag='git tag -n1'
 alias gitst='git status --ignored'
 alias gitbr='git branch -vv'
+alias gitc="git commit -am $1"
+alias gitp="git push && git push --tags"
+
+# Alias
+alias update-alias='curl -sS https://raw.githubusercontent.com/pacroy/bash-init/master/init.sh | bash - && source ~/alias.sh'
