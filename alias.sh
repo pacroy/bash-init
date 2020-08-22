96e2874af29f5107c57b883e1c6995a2740a68ea
Par <pacroy@gmail.com>
2020-08-22 11:23:39 +0700

    Expand functions to multi-lines

--- alias.sh
+++ alias.sh
@@ -1,14 +1,20 @@
 # Azure CLI
 alias azset='az account set -s'
 alias azlist='az account list -o table'
-azaks() { az aks get-credentials --resource-group $1 --name $2; }
+azaks() { 
+    az aks get-credentials --resource-group $1 --name $2
+}
 
 # Kubernetes CLI
 alias kc='kubectl'
 alias kclist='kc config get-contexts'
 alias kcuse='kc config use-context'
-kcsetns() { kc config set-context $(kc config current-context) --namespace $1; }
-kcstatus() { kc rollout status deploy $1; }
+kcsetns() { 
+    kc config set-context $(kc config current-context) --namespace $1
+}
+kcstatus() { 
+    kc rollout status deploy $1
+}
 
 # Terraform
 alias tf='terraform'
@@ -28,11 +34,17 @@ alias gitl='git log --oneline --decorate --all'
 alias gitt='git tag -n1'
 alias gits='git status --ignored'
 alias gitb='git branch -vv'
-gitc() { git commit -am "$1"; }
 alias gitp='git push && git push --tags'
 alias gitd='git diff'
 alias gita='git add .'
-gitz() { gita && git commit -m "$1" && gitp; }
+gitc() { 
+    git commit -am "$1" 
+}
+gitz() { 
+    gita
+    git commit -m "$1"
+    gitp 
+}
 
 # Alias
 alias update-alias='curl -sS https://raw.githubusercontent.com/pacroy/bash-init/master/init.sh | bash - && source ~/alias.sh'
